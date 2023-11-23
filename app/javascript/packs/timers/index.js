15ac2a8bf52ab2301c173b51e9e629f9f58dffc1
your name <your-email@example.com>
2023-11-23 12:03:14 +0900

    axios追記

--- app/javascript/packs/timers/index.js
+++ app/javascript/packs/timers/index.js
@@ -39,8 +39,28 @@ document.getElementById("start").addEventListener("click", startTimer);
 document.getElementById("stop").addEventListener("click", stopTimer);
 document.getElementById("reset").addEventListener("click", resetTimer);
 
-// ↓受け渡したいデータを格納
-const data = { timers: createImageBitmap, user_id: "<%= current_user.id if logged_in? %>", beat_id: "<%= @beat.id %>" }
+// ユーザーIDとビートIDを取得
+const userId = document.getElementById('user-info').dataset.userId;
+const beatId = document.getElementById('beat-info').dataset.beatId;
 
-// ↓送信したいURLと渡したいデータを指定
-axios.post('/scores', data);
+// タイマーが停止されたときに呼び出される関数
+function saveTimerDataToDB() {
+  const data = {
+    user_id: userId,
+    beat_id: beatId,
+    hours: hours,
+    minutes: minutes,
+    seconds: seconds
+  };
+
+  axios.post('/scores', data)
+    .then(response => {
+      console.log('Data saved to database:', response.data);
+      // サーバーにデータが保存された後の処理
+    })
+    .catch(error => {
+      console.error('Error saving data:', error);
+      // エラー時の処理
+    });
+}
+
